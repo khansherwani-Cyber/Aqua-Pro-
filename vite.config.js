01eeebeea4bcf37c5b102caf53fb7267562f6527
Muhammad Fouz Khan <khansherwani169@gmail.com>
2026-06-18 15:25:29 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-export default defineConfig({ plugins: [react()] });
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    target: 'esnext',  // This allows top-level await
+  },
+  esbuild: {
+    target: 'esnext',  // This also allows top-level await
+  },
+});
